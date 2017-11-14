832f6e9658d520e417d727095f28c4308e85d2d7
warnerotz <adrichony@gmail.com>
2017-11-14 14:57:50 +0100

    14/11/2017

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,26 @@
 import _ from 'lodash';
 
 
-import {start} from './game.js';
 
-_.times(3, (i)=>{
-	console.log(i);
-});
+window.onload = function () {
+   //las imagnes claras solo dragstart y dragend
+   var bulbasur = document.getElementById("bulbasur");
+    bulbasur.addEventListener("dragstart", arrastrar, false);
+    bulbasur.addEventListener("dragend", finArrastrar, false);
+    
+    
+   //dragover drop dragenter dragleave las oscuras
 
+    
+    function arrastrar(ev){
+        
+        ev.dataTransfer.setData("text", ev.target.id)
+        console.log(ev.target.id);
+    }
+    
+    function finArrastrar(ev){
+        
+        
+    }
+}
 
-
-start();
